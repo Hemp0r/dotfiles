af4ce0c1b3dfaddf23630b5193d0e9714b7bf94d
Pierre Hempe <pierre.hempe@dmk.de>
2023-03-13 11:42:40 +0000

    added brew

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 cp vscode/settings.json ~/.local/share/code-server/User/settings.json
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/coder/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+brew install gcc
