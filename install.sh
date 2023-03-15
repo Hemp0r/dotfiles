1503c7bc37037d8d3aafbaebeeab98df10582d39
Pierre Hempe <pierre.hempe@dmk.de>
2023-03-15 20:29:29 +0000

    added ohmyzsh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cp vscode/settings.json ~/.local/share/code-server/User/settings.json
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/coder/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo apt-get install build-essential
 brew install gcc
+
