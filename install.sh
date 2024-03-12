bda6113ae907695ff0bab25939466597de7db68f
Pierre Hempe <hemp0r@mailbox.org>
2024-03-12 23:05:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ PACKAGE="📦"
 # ---- CONFIGURE VS CODE SETTINGS ----
 echo -e "${GREEN}Configuring VS Code settings...${RESET}"
 mkdir -p ~/.local/share/code-server/User/ && cp vscode/settings.json ~/.local/share/code-server/User/settings.json &>/dev/null
+
+if which code-server > /dev/null; then
+   code-server --install GitHub.Copilot
+else
+   echo "Unable to install Extensions"
+fi
